74e7bbfff65a88bfd6cb6a1fced8e1cfd87e9915
Skuid <ltcrain2011@gmail.com>
2025-11-26 19:51:12 -0800

    Initial Commit

---
+++ src/main/java/com/skuid/airway/AirwayClient.java
@@ -0,0 +1,11 @@
+package com.skuid.airway;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AirwayClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
